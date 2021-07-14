40d50103ef12950d57d57c65a7ec63953b220e52
Conor R <conor.riches@gmail.com>
2021-07-14 23:28:49 +0100

    Verify HMAC and post to telegram

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
+const axios = require("axios");
+const crypto = require("crypto");
+
+const telegramToken = process.env.TelegramToken;
+const chatID = process.env.ChatID;
+const HMACSecret = process.env.HMACSecret;
 
 exports.newDiscourseTopic = (req, res) => {
-  const topic = req.body.topic;
+  if (!telegramToken || !chatID || !HMACSecret) {
+    res.status(400).send("process envs not set");
+    return;
+  }
 
+  // Check the signature
+  const signature = req.headers["x-discourse-event-signature"].split("=")[1];
+  const hmac = crypto
+    .createHmac("sha256", HMACSecret)
+    .update(JSON.stringify(req.body))
+    .digest("hex");
+
+  if (hmac !== signature) {
+    res.status(400).send("invalid HMAC - did you set the secret?");
+    return;
+  }
+
+  const { topic } = req.body;
   if (topic) {
-    const title = topic.title;
+    const { title, id, visible, posts_count, closed, archetype } = topic;
 
-    res.status(200).send(topic);
+    if (closed || !visible || posts_count > 1 || archetype !== "regular") {
+      res.status(200).send("Not worthy of a notification");
+      return;
+    }
+
+    axios
+      .get(`https://api.telegram.org/bot${telegramToken}/sendMessage`, {
+        params: {
+          chat_id: chatID,
+          text:
+            `✒️ <i>New post on the forum:</i> \n\n` +
+            `<b>${title}</b> \n\n` +
+            `<a href="https://list.hacman.org.uk/t/${id}"> 🔗 View and reply on the forum</a>`,
+          parse_mode: "HTML",
+        },
+      })
+      .then(() => {
+        res.status(200).send(telegramToken);
+      })
+      .catch((e) => {
+        res.status(500).send();
+      });
   } else {
-    res.status(400);
+    res.status(400).send();
   }
 };
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Discourse to Telegram for topics
+Fires a notification to Telegram when a new topic is posted to Discourse.
+
+## Runtime environment variables
+* TelegramToken
+* ChatID
